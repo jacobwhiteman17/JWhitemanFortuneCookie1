08e4e0cc8d3548e0d6735fc1ff9dae1862373c7e
jacobwhiteman17 <j1whiteman@student.bridgew.edu>
2019-09-17 23:35:04 -0400

    Probably the final commit. Made new function that gets the random saying.

--- src/fortuneCookie.java
+++ src/fortuneCookie.java
@@ -5,25 +5,28 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 public class fortuneCookie {
     static String fileName = "src/sayings.txt";//the file of sayings
-    static Scanner reader = new Scanner(System.in);
-    static Random randGen = new Random();
+    static Scanner reader = new Scanner(System.in);//scanner
+    static Random randGen = new Random();//random
+    static String[] sayings = new String[0];//creates my empty array
 
-    //public String theLoop(){
+    public static String getSaying(){
+        String newSaying= "";//empty saying we can plug stuff into
 
-    //}
+        int randNum = randGen.nextInt(sayings.length);//take a random number from array
+        newSaying = sayings[randNum];//updates newSaying to take one of the items in the list into the " "
+        return newSaying;//returns the new saying
+    }
 
     public static void main (String[] args)throws IOException{
-          System.out.println(Paths.get(fileName));
-          String allSayings = Files.readString(Paths.get(fileName));//reading the list
-          String[] sayings = allSayings.split("\n");
+        String allSayings = Files.readString(Paths.get(fileName));//reading the text file
+        sayings = allSayings.split("\n");//updates array to split between enter keys
 
-        System.out.println("How many sayings would you like to read? ");
-        var userChoice = reader.nextInt();
+        System.out.println("How many sayings would you like to read? ");//simple
+        var userChoice = reader.nextInt();//user input
 
-        for (int i=userChoice; i>0;i--){
-            var randSaying = randGen.nextInt(sayings.length-1);
-            System.out.println(sayings[randSaying]);
-            System.in.read();
+        for (int i=userChoice; i>0;i--){//for the userChoice, when its greater than 0, subtract
+            System.out.println(getSaying());//print our getSaying function
+            System.in.read();//allows user to hit enter to continue. Had an issue with the nextLine() so I eventually found this
 
 
         }
