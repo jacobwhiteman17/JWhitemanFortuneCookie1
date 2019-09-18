1d6382f17f52a98c985c72cdef23033b13e7dc50
jacobwhiteman17 <j1whiteman@student.bridgew.edu>
2019-09-17 22:46:35 -0400

    testing something in code

--- src/fortuneCookie.java
+++ src/fortuneCookie.java
@@ -4,27 +4,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 public class fortuneCookie {
+    static String fileName = "src/sayings.txt";//the file of sayings
+    static Scanner reader = new Scanner(System.in);
+    static Random randGen = new Random();
+
+    //public String theLoop(){
+
+    //}
+
     public static void main (String[] args)throws IOException{
-          var fileName = "src/sayings.txt";//the file of sayings
-          var reader = new Scanner(System.in);
-          var randGen = new Random();
           System.out.println(Paths.get(fileName));
           String allSayings = Files.readString(Paths.get(fileName));//reading the list
           String[] sayings = allSayings.split("\n");
 
         System.out.println("How many sayings would you like to read? ");
-
         var userChoice = reader.nextInt();
+
         for (int i=userChoice; i>0;i--){
-            var randSaying = randGen.nextInt(21);
+            var randSaying = randGen.nextInt(sayings.length-1);
             System.out.println(sayings[randSaying]);
-            var keepGoing = reader.nextLine();
+            System.in.read();
 
 
         }
-
-
-
 
     }
 }
